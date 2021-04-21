049806e0905011ae351431111e18b0079cbe85d5
Renan Muniz <fmrenan@outlook.com>
2021-04-21 15:15:05 -0300

    Home finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,8 +33,14 @@ export default function Home({latestEpisodes, allEpisodes} : HomeProps) {
         <ul>
           {latestEpisodes.map(episode => {
             return(
-              <li key={episode.id}>{episode.title}
-                <Image width={192} height={192} src={episode.thumbnail} alt={episode.title} />
+              <li key={episode.id}>
+                <Image 
+                  width={192} 
+                  height={192} 
+                  src={episode.thumbnail} 
+                  alt={episode.title} 
+                  objectFit="cover"
+                />
                 
                 <div className={styles.episodeDetails}>
                   <a href="#">{episode.title}</a>
@@ -53,7 +59,49 @@ export default function Home({latestEpisodes, allEpisodes} : HomeProps) {
       </section>
 
       <section className={styles.allEpisodes}>
-        
+        <h2>Todos Episódios</h2>
+
+        <table cellSpacing={0}>
+          <thead>
+            <th></th>
+            <th>PodCast</th>
+            <th>Integrantes</th>
+            <th>Data</th>
+            <th>Duração</th>
+            <th></th>
+          </thead>
+
+          <tbody>
+            {allEpisodes.map(episode => {
+              return (
+                <tr key={episode.id}>
+                  <td style={{width: 72}}>
+                    <Image 
+                      width={120}
+                      height={120}
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                      objectFit="cover"
+                    />
+                  </td>
+                  <td>
+                    <a href="#">{episode.title}</a>
+                  </td>
+
+                  <td>{episode.members}</td>
+                  <td style={{width: 100}}>{episode.publishedAt}</td>
+                  <td>{episode.durationAsString}</td>
+
+                  <td>
+                    <button type="button">
+                      <img src="/play-green.svg" alt="Tocar Episódio"/>
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </section>
     </div>
   )
