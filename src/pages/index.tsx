6310c2e7f8cf0a39e481996c2ec743440a8c8b56
Renan Muniz <fmrenan@outlook.com>
2021-04-22 23:29:38 -0300

    changed backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -118,13 +118,7 @@ export default function Home({latestEpisodes, allEpisodes} : HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { data } = await api.get('episodes',{
-    params:{
-      _limit: 12,
-      _sort: 'published_at',
-      order: 'desc'
-    }
-  })
+  const { data } = await api.get('episodes')
 
   const episodes = data.map(episode => {
     return{
